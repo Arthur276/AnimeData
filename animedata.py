198ad90d3476a612f69e0f1efa14bf42b4423e36
Arthur <arthur.cott@free.fr>
2022-11-04 10:26:08 +0100

    Migration des fonction JSON depuis AD
    Toutes les fonctions interagissant avec les fichiers JSON ont été migrées vers le module AnimeData.
    D'autres changements mineurs tels que :
    - Préparation de la publication sur Pypi
    - Mise à jour de la librairie AD

--- animedata.py
+++ animedata.py
@@ -1,11 +1,54 @@
 import json
+import urllib.request
 
-ad_dict={"url" : "https://raw.githubusercontent.com/Arthur276/AnimeData/main/",
-            "nom_fichier_distant" : "main_anime_list.json",
-            "nom_fichier_local" : "animedata_list.json",
-            "dict_nom_anime_clé" : "nom_anime",
-            "dict_episodes_clé" : "saisons_episodes",
-            "nom_fichier_personalisé" : "animetime_local.json",
-            "dict_episode_duree_clé" : "duree_episode",
-            "dict_episode_date_sortie_clé" : "date_sortie_episode",
+ad_version = "ADV-0.1"
+print("Version du script python d'AnimeData : ", ad_version)
+
+animedata = {"url" : "https://raw.githubusercontent.com/Arthur276/AnimeData/main/",
+            "nom_fichier_source" : "animedata_source.json",
+            "nom_fichier_local" : "animedata_local.json",
+            "dict_nom_anime" : "nom_anime",
+            "dict_saisons_episodes" : "saisons_episodes",
+            "dict_duree_episode" : "duree_episode",
+            "dict_date_sortie_episode" : "date_sortie_episode",
             "dict_nom_episode" : "nom_episode"}
+
+def maj_anime_liste():
+    """Télécharge le fichier source d'AnimeData contenant les données des animés"""
+    #STATUS : TEST
+    urllib.request.urlretrieve(animedata["url"] + animedata["nom_fichier_source"],animedata["nom_fichier_source"])
+    with open(animedata["nom_fichier_source"],"r",encoding = "utf-8") as animedata_json:
+        main_dict = json.load(animedata_json)
+        print("Données d'AnimeData téléchargées !")
+        print("Version AnimeData du fichier téléchargé :" + main_dict["ANIMEDATA-METADATA"]["animedata_version"])
+        print("Voici les animés disponible en ligne :")
+        for element in main_dict.values():
+            if element["type"] == "anime":
+                print(element[animedata["dict_nom_anime"]])
+
+def sauv_json(anime_dict):
+    """Sauvegarde les données des animés contenues dans un dictionnaire dans un fichier JSON personalisé"""
+    #STATUS : BETA
+    with open(animedata["nom_fichier_local"],"w",encoding = "utf-8") as local_json:
+        for anime in anime_dict.values():
+            if anime["type"] == "anime":
+                dict_ok = True
+            else:
+                dict_ok = False
+                break
+        if dict_ok :
+            json_dict = {"ANIMEDATA-METADATA":{"type" : "metadata","animedata_version" : ad_version}, **anime_dict}
+        else:
+            print("Dictionnaire transmis non conforme à la sauvegarde")
+        json.dump(obj = json_dict, fp = local_json,ensure_ascii = False)
+
+def get_json_dict(ad_source = False):
+    """Récupère le dictionnaire contenant les données depuis le fichier local d'AnimeData ou un fichier personalisé"""
+    #STATUS : BETA
+    if ad_source :
+        fichier_cible = animedata["nom_fichier_source"]
+    else:
+        fichier_cible = animedata["nom_fichier_local"]
+    with open(fichier_cible,"r",encoding ="utf-8") as animedata_json:
+        anime_dict = json.load(animedata_json)
+    return anime_dict
