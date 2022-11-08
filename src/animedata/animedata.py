99196bd3180adc9db1803f100a80ec4bfe59e805
Arthur <arthur.cott@free.fr>
2022-11-08 18:13:32 +0100

    Ajout du "dev mode"
    Ajout "dev mode":
    - décomposition de l'url dans animedata{}
    - ajout de la possibilité de choisir entre le branche de développement et la branche principale dans maj_anime_list()
    
    - passage version 0.1.2

--- src/animedata/animedata.py
+++ src/animedata/animedata.py
@@ -4,7 +4,10 @@ import urllib.request
 ad_version = "ADV-0.1"
 print("Version du script python d'AnimeData : ", ad_version)
 
-animedata = {"url" : "https://raw.githubusercontent.com/cassphir/AnimeData/main/",
+animedata = {"dev_branch" : "dev-0.1" ,
+            "main_branch" : "main" ,
+            "file_path" : "/src/animedata/animedata_source.json"
+            "repository_url" : "https://raw.githubusercontent.com/cassphir/AnimeData/",
             "nom_fichier_source" : "animedata_source.json",
             "nom_fichier_local" : "animedata_local.json",
             "dict_nom_anime" : "nom_anime",
@@ -13,10 +16,13 @@ animedata = {"url" : "https://raw.githubusercontent.com/cassphir/AnimeData/main/
             "dict_date_sortie_episode" : "date_sortie_episode",
             "dict_nom_episode" : "nom_episode"}
 
-def maj_anime_liste():
+def maj_anime_liste(dev_mode = False):
     """Télécharge le fichier source d'AnimeData contenant les données des animés"""
     #STATUS : TEST
-    urllib.request.urlretrieve(animedata["url"] + animedata["nom_fichier_source"],animedata["nom_fichier_source"])
+    if dev_mode:
+        urllib.request.urlretrieve(animedata["repository_url"] + animedata["dev_branch"] + animedata["file_path"],animedata["nom_fichier_source"])
+    else:
+        urllib.request.urlretrieve(animedata["repository_url"] + animedata["main_branch"] + animedata["file_path"],animedata["nom_fichier_source"])
     with open(animedata["nom_fichier_source"],"r",encoding = "utf-8") as animedata_json:
         main_dict = json.load(animedata_json)
         print("Données d'AnimeData téléchargées !")
