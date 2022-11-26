8dc9c06d39f22e7ac750e02586b0d4f4f3a60a45
Arthur <arthur.cott@free.fr>
2022-11-26 11:45:10 +0100

    Changes : - Code translation started - Code quality improvements

--- animedata.py
+++ animedata.py
@@ -2,63 +2,73 @@ import json
 import urllib.request
 import tomli
 
-with open("./pyproject.toml", mode="rb") as pypr:
+with open("../animedata/pyproject.toml", mode="rb") as pypr:
     ad_version = tomli.load(pypr)["project"]["version"]
-print("Version du script python d'AnimeData : ", ad_version)
+print("AnimeData script version : ", ad_version)
 dev_mode = False
-animedata = {"dev_branch" : "dev-0.1" ,
-            "main_branch" : "main" ,
-            "file_path" : "/src/animedata/animedata_source.json",
-            "repository_url" : "https://raw.githubusercontent.com/cassphir/animedata/",
-            "nom_fichier_source" : "animedata_source.json",
-            "nom_fichier_local" : "animedata_local.json",
-            "dict_nom_anime" : "nom_anime",
-            "dict_saisons_episodes" : "saisons_episodes",
-            "dict_duree_episode" : "duree_episode",
-            "dict_date_sortie_episode" : "date_sortie_episode",
-            "dict_nom_episode" : "nom_episode"}
+ad_table = {
+    "dev_branch": "dev-0.1/",
+    "main_branch": "main/",
+    "repository_url":
+    "https://raw.githubusercontent.com/cassphir/ad_table/",
+    "source_file_name": "animedata_source.json",
+    "local_file_name": "animedata_local.json",
+    "key_anime_name": "nom_anime",
+    "key_seasons_episodes": "saisons_episodes",
+    "key_episode_duration": "duree_episode",
+    "key_episode_release_date": "date_sortie_episode",
+    "key_episode_name": "nom_episode"}
 
 
-def maj_anime_lib():
+def update_anime_lib():
     """Met à jour la librairie AnimeData depuis Github"""
-    #STATUS : OK
+    # STATUS : OK
     if dev_mode:
-        urllib.request.urlretrieve(animedata["repository_url"] + animedata["dev_branch"] + animedata["file_path"],animedata["nom_fichier_source"])
+        urllib.request.urlretrieve(
+            ad_table["repository_url"] +
+            ad_table["dev_branch"] +
+            ad_table["source_file_name"],
+            ad_table["source_file_name"])
     else:
-        urllib.request.urlretrieve(animedata["repository_url"] + animedata["main_branch"] + animedata["file_path"],animedata["nom_fichier_source"])
-    with open(animedata["nom_fichier_source"],"r",encoding = "utf-8") as animedata_json:
-        main_dict = json.load(animedata_json)
-        print("Version AnimeData du fichier téléchargé :" + main_dict["ANIMEDATA-METADATA"]["animedata_version"],"#" + main_dict["ANIMEDATA-METADATA"]["lib_subversion"])
-        print("Voici les animés disponible en ligne :")
+        urllib.request.urlretrieve(
+            ad_table["repository_url"] +
+            ad_table["main_branch"] +
+            ad_table["source_file_name"],
+            ad_table["source_file_name"])
+    with open(ad_table["source_file_name"], encoding="utf-8") as ad_json:
+        main_dict = json.load(ad_json)
+        print("AnimeData library version :" +
+              main_dict["ANIMEDATA-METADATA"]["animedata_version"],
+              "#" +
+              main_dict["ANIMEDATA-METADATA"]["lib_subversion"])
+        print("Animes downloaded from Github :")
         for element in main_dict.values():
             if element["type"] == "anime":
-                print(element[animedata["dict_nom_anime"]])
+                print(element[ad_table["key_anime_name"]])
 
 
 def sauv_json(anime_dict):
     """Sauvegarde les données des animés contenues dans un dictionnaire dans un fichier JSON personalisé"""
-    #STATUS : OK
-    with open(animedata["nom_fichier_local"],"w",encoding = "utf-8") as local_json:
+    # STATUS : OK
+    with open(ad_table["local_file_name"], "w", encoding="utf-8") as local_json:
         for anime in anime_dict.values():
-            if anime["type"] == "anime":
-                dict_ok = True
-            else:
-                dict_ok = False
-                break
-        if dict_ok :
-            json_dict = {"ANIMEDATA-METADATA":{"type" : "metadata","animedata_version" : ad_version}, **anime_dict}
-        else:
-            print("Dictionnaire transmis non conforme à la sauvegarde")
-        json.dump(obj = json_dict, fp = local_json,ensure_ascii = False)
+            if anime["type"] != "anime":
+                raise SyntaxError("dictionnary is not correctly formatted")
+        json_dict = {
+            "ANIMEDATA-METADATA": {
+                "type": "metadata",
+                "animedata_version": ad_version},
+            **anime_dict}
+        json.dump(obj=json_dict, fp=local_json, ensure_ascii=False)
 
 
-def get_json_dict(ad_source = False):
+def get_json_dict(ad_source=False):
     """Récupère le dictionnaire contenant les données depuis le fichier local d'AnimeData ou un fichier personalisé"""
-    #STATUS : OK
-    if ad_source :
-        fichier_cible = animedata["nom_fichier_source"]
+    # STATUS : OK
+    if ad_source:
+        target_file = ad_table["source_file_name"]
     else:
-        fichier_cible = animedata["nom_fichier_local"]
-    with open(fichier_cible,"r",encoding ="utf-8") as animedata_json:
-        anime_dict = json.load(animedata_json)
+        target_file = ad_table["local_file_name"]
+    with open(target_file, "r", encoding="utf-8") as ad_json:
+        anime_dict = json.load(ad_json)
     return anime_dict
