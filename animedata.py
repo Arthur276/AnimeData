22c36c9319b702eeb269b24c56d95597c7d77969
arthur <arthur.cott@free.fr>
2022-12-25 21:19:11 +0100

    check_dict implemented, minor correction
    New feature : check_dict:
    - checks if a dictionnary is compatible with animedata.
    - analyses and creates a list of the corrupted dict keys.
    - returns a corrected dict based on the corrupted keys list.
    Correction :
    - dev_mode implementation in update_anime_lib has been improved.

--- animedata.py
+++ animedata.py
@@ -29,18 +29,15 @@ print("AnimeData script version : ", ad_version)
 def update_anime_lib():
     """Download and replaces animedata_source.json file from Github."""
     # STATUS : OK
-    if dev_mode:
-        urllib.request.urlretrieve(
-            ad_table["repository_url"] +
-            ad_table["dev_branch"] +
-            ad_table["source_file_name"],
-            ad_table["source_file_name"])
+    if dev_mode :
+        target_branch = ad_table["dev_branch"]
     else:
-        urllib.request.urlretrieve(
-            ad_table["repository_url"] +
-            ad_table["main_branch"] +
-            ad_table["source_file_name"],
-            ad_table["source_file_name"])
+        target_branch = ad_table["main_branch"] 
+    urllib.request.urlretrieve(
+        ad_table["repository_url"] +
+        target_branch +
+        ad_table["source_file_name"],
+        os.path.join(dir_path,ad_table["source_file_name"]))
     with open(os.path.join(dir_path,ad_table["source_file_name"]), encoding="utf-8") as ad_json:
         main_dict = json.load(ad_json)
         print("AnimeData library version :" +
@@ -59,27 +56,25 @@ def save_json(anime_dict: dict):
     Args:
         anime_dict (dict): Dictionnary containing anime's data.
             Must be formatted with multi_anime_dict.
-
-    Raises:
-        SyntaxError: Error when the dictionnary is not correctly formatted
     """
     # STATUS : OK
     with open(os.path.join(dir_path,ad_table['local_file_name']),
               "w",
               encoding="utf-8") as local_json:
-        for anime in anime_dict.values():
-            if anime["type"] != "anime":
-                raise SyntaxError("The dictionnary is not correctly \
-                                  formated. Use format_dict()")
+        if not check_dict(anime_dict)[0]:
+            warnings.warn(f"The dictionnary contains one or several \
+corrupted key, ignoring it. Corrupted keys : {check_dict(anime_dict)[2]}")
+        correct_dict = check_dict(anime_dict)[1]
         json_dict = {
             "ANIMEDATA-METADATA": {
                 "type": "metadata",
                 "animedata_version": ad_version},
-            **anime_dict}
-        json.dump(obj=json_dict, fp=local_json, ensure_ascii=False)
+            **correct_dict
+            }
+        json.dump(obj=json_dict, fp=local_json, ensure_ascii=False, indent= 4)
 
 
-def load_json_dict(ad_source: bool = False) -> dict:
+def load_json(ad_source: bool = False) -> dict:
     """Load data from a json file containing animes' data.
 
     Args:
@@ -99,22 +94,35 @@ def load_json_dict(ad_source: bool = False) -> dict:
         anime_dict = json.load(ad_json)
     return anime_dict
 
-def check_dict(anime_dict:dict,correct = False) -> bool:
+def check_dict(anime_dict:dict) -> tuple:
+    """Check if the dictionnary is compatible with animedata's environment.
+
+    Args:
+        anime_dict (dict): dictionnary to check.
+
+    Returns:
+        tuple: tuple containing three main elements:
+            - bool if the dictionnary is fully compatible.
+            - corrected dictionnary.
+            - list containing the corrupted keys of the original dict.
+    """
     corrupted_keys = []
     dict_valid = True
+    correct_dict = anime_dict.copy()
     for element in anime_dict.keys():
         dict_element = anime_dict[element]
         try:
             if dict_element["type"] == "anime":
                 if dict_element.get(ad_table["key_anime_name"]) != element:
                     corrupted_keys.append(element)
-                    dict_valid = False
-            elif dict_element.get("type") == "metadata":
-                if element != "ANIMEDATA-METADATA":
-                    corrupted_keys.append(element)
-                    dict_valid = False
+            elif dict_element["type"] == "metadata":
+                corrupted_keys.append(element)
+            else:
+                corrupted_keys.append(element)
         except KeyError:
             corrupted_keys.append(element)
-            if correct:
-                del anime_dict[element]
-    return dict_valid, anime_dict, corrupted_keys
+    if len(corrupted_keys) != 0:
+        for corrupted_anime in corrupted_keys:
+            del correct_dict[corrupted_anime]
+        dict_valid = False
+    return dict_valid, correct_dict, corrupted_keys
