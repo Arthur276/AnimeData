4c357eda910411ab054cff1091feba368a05935e
arthur <arthur.cott@free.fr>
2023-01-12 22:18:43 +0100

    Added testing for checkdict
    Correction :
    - Added testing for checkdict function
    - Complete testing for other function

--- ad_test.py
+++ ad_test.py
@@ -2,7 +2,13 @@ import animedata as ad
 
 if __name__ == "__main__":
     ad.dev_mode = True
-    #ad.update_anime_lib()
-    json_dict = ad.load_json_dict(True)
-    print(json_dict)
-    ad.save_json(json_dict)
+    ad.update_anime_lib()
+    test_json = ad.load_json(True)
+    del test_json["ANIMEDATA-METADATA"]
+    print(ad.check_dict(test_json))
+    ad.save_json(test_json)
+    test_json_2 = ad.load_json(False)
+    del test_json_2["ANIMEDATA-METADATA"]
+    print(ad.check_dict(test_json_2))
+    print(test_json_2 == test_json)
+    
