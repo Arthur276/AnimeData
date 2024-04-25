364ee9e62ab3066b409c33bceec05e0489be0458
arthur <arthur.cott@free.fr>
2024-04-25 15:47:02 +0200

    fixed incorrect path for animedata_local.json and animedata_source.json in metadata.ad_table

--- animedata/common/metadata.py
+++ animedata/common/metadata.py
@@ -4,15 +4,14 @@ import animedata
 
 ad_version = importlib.metadata.version("animedata")
 dir_path = os.path.dirname(animedata.__file__)
+repository_url = "https://raw.githubusercontent.com/swarthur/animedata/"
 ad_table = {
-    "repository_url":
-    "https://raw.githubusercontent.com/swarthur/animedata/",
     "source_file_name": "animedata_source.json",
     "local_file_name": "animedata_local.json",
     "source_file_path": os.path.join(dir_path,
-                                     "resources\\animedata_source.json"),
+                                     "resources/animedata_source.json"),
     "local_file_path": os.path.join(dir_path,
-                                    "resources\\animedata_local.json"),
+                                    "resources/animedata_local.json"),
     "anime_name": "anime_name",
     "seasons": "seasons",
     "episode_duration": "episode_duration",
